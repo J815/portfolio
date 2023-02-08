42ee7954947474bb89fa906ead9ae18d7ad3e79f
J815 <58225317+J815@users.noreply.github.com>
2023-02-08 08:18:55 +0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'docs')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
